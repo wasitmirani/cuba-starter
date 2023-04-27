dacd2d3e9cb8c0e07e9ae9be7b16cc66bfeb120f
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2023-04-27 17:18:23 +0500

    first commit

---
+++ resources/views/layouts/backend/master.blade.php
@@ -0,0 +1,105 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Cuba admin is super flexible, powerful, clean &amp; modern responsive bootstrap 5 admin template with unlimited possibilities.">
+    <meta name="keywords" content="admin template, Cuba admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="author" content="pixelstrap">
+    <link rel="icon" href="../assets/images/favicon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../assets/images/favicon.png" type="image/x-icon">
+    <title>Cuba - Premium Admin Template</title>
+    <!-- Google font-->
+    <link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900&amp;display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/font-awesome.css')}}">
+    <!-- ico-font-->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/icofont.css')}}">
+    <!-- Themify icon-->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/themify.css')}}">
+    <!-- Flag icon-->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/flag-icon.css')}}">
+    <!-- Feather icon-->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/feather-icon.css')}}">
+    <!-- Plugins css start-->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/slick.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/slick-theme.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/scrollbar.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/animate.css')}}">
+    <!-- Plugins css Ends-->
+    <!-- Bootstrap css-->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/bootstrap.css')}}">
+    <!-- App css-->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}">
+    <link id="color" rel="stylesheet" href="{{asset('assets/css/color-1.css')}}" media="screen">
+    <!-- Responsive css-->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/responsive.css')}}">
+  </head>
+  <body onload="startTime()">
+    <!-- loader starts-->
+    <div class="loader-wrapper">
+      <div class="loader-index"> <span></span></div>
+      <svg>
+        <defs></defs>
+        <filter id="goo">
+          <fegaussianblur in="SourceGraphic" stddeviation="11" result="blur"></fegaussianblur>
+          <fecolormatrix in="blur" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="goo"> </fecolormatrix>
+        </filter>
+      </svg>
+    </div>
+    <!-- loader ends-->
+    <!-- tap on top starts-->
+    <div class="tap-top">
+    <i data-feather="chevrons-up"></i>
+    </div>
+    <!-- tap on tap ends-->
+    <!-- page-wrapper Start-->
+    <div id="app">
+        @yield('content')
+    </div>
+
+    <!-- latest jquery-->
+    <script src="{{asset('assets/js/jquery.min.js')}}"></script>
+    <!-- Bootstrap js-->
+    <script src="{{asset('assets/js/bootstrap/bootstrap.bundle.min.js')}}"></script>
+    <!-- feather icon js-->
+    <script src="{{asset('assets/js/icons/feather-icon/feather.min.js')}}"></script>
+    <script src="{{asset('assets/js/icons/feather-icon/feather-icon.js')}}"></script>
+    <!-- scrollbar js-->
+    <script src="{{asset('assets/js/scrollbar/simplebar.js')}}"></script>
+    <script src="{{asset('assets/js/scrollbar/custom.js')}}"></script>
+    <!-- Sidebar jquery-->
+    <script src="{{asset('assets/js/config.js')}}"></script>
+
+    <!-- Plugins JS start-->
+    <script src="{{asset('assets/js/sidebar-menu.js')}}"></script>
+    <script src="{{asset('assets/js/clock.js')}}"></script>
+    <script src="{{asset('assets/js/slick/slick.min.js')}}"></script>
+    <script src="{{asset('assets/js/slick/slick.js')}}"></script>
+    <script src="{{asset('assets/js/header-slick.js')}}"></script>
+    <script src="{{asset('assets/js/chart/apex-chart/apex-chart.js')}}"></script>
+    <script src="{{asset('assets/js/chart/apex-chart/stock-prices.js')}}"></script>
+    <script src="{{asset('assets/js/chart/apex-chart/moment.min.js')}}"></script>
+    <script src="{{asset('assets/js/notify/bootstrap-notify.min.js')}}"></script>
+    <script src="{{asset('assets/js/dashboard/default.js')}}"></script>
+    <script src="{{asset('assets/js/notify/index.js')}}"></script>
+    <script src="{{asset('assets/js/typeahead/handlebars.js')}}"></script>
+    <script src="{{asset('assets/js/typeahead/typeahead.bundle.js')}}"></script>
+    <script src="{{asset('assets/js/typeahead/typeahead.custom.js')}}"></script>
+    <script src="{{asset('assets/js/typeahead-search/handlebars.js')}}"></script>
+    <script src="{{asset('assets/js/typeahead-search/typeahead-custom.js')}}"></script>
+    <script src="{{asset('assets/js/height-equal.js')}}"></script>
+    <script src="{{asset('assets/js/animation/wow/wow.min.js')}}"></script>
+    <!-- Plugins JS Ends-->
+    <!-- Theme js-->
+    <script src="{{asset('assets/js/script.js')}}"></script>
+    <script src="{{asset('assets/js/theme-customizer/customizer.js')}}"></script>
+    <script src="{{mix('js/app.js')}}" defer></script>
+
+    <!-- Plugin used-->
+    <script>new WOW().init();</script>
+  </body>
+</html>
